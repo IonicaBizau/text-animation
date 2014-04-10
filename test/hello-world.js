98d031f13d66a2569567c3c0aeaee23f16c0dd48
Ionică Bizău <bizauionica@yahoo.com>
2014-04-10 11:21:45 +0300

    Updated test

--- test/hello-world.js
+++ test/hello-world.js
@@ -1,8 +1,15 @@
-var TextAnimation = require ("text-animation");
+var TextAnimation = require ("../index")
+  , Util          = require ("util")
+  ;
 
 TextAnimation.start({
     animate: "down-up"
   , text: "Hello World!"
   , delay: 100
   , size: "40x30"
+  , end: function (frames) {
+        Util.print("\u001b[2J\u001b[0;0H");
+        console.log(frames[frames.length - 1]);
+        setTimeout (function () {}, 1000);
+    }
 });
