b19f78a8e022a07bb55e074712d35bc0a036020d
Ionică Bizău <bizauionica@yahoo.com>
2014-12-17 21:55:14 +0200

    Simplified the test file

--- test/index.js
+++ test/index.js
@@ -1,13 +1,2 @@
-var TextAnimation = require("../index");
-
-TextAnimation.start({
-    animate: "down-up"
-  , text: "Hello World!"
-  , delay: 100
-  , size: "40x30"
-  , end: function (frames) {
-        Util.print("\u001b[2J\u001b[0;0H");
-        console.log(frames[frames.length - 1]);
-        setTimeout (function () {}, 1000);
-    }
-});
+var TextAnimation = require("../lib");
+TextAnimation("Hello World!");
