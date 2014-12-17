34f590ce7ada6e8c5f92e7537fc02a48c300b832
Ionică Bizău <bizauionica@yahoo.com>
2014-12-17 21:46:48 +0200

    Refactored the code.

--- index.js
+++ index.js
@@ -1,99 +1,85 @@
-// dependencies
-var Jasciimo = require('jasciimo').Figlet
-  , Overlap = require ("overlap")
-  , AsciiFrames = require ("ascii-frames")
+// Dependencies
+var Figlet = require("figlet")
+  , Overlap = require("overlap")
+  , AsciiFrames = require("ascii-frames")
+  , CliBox = require("cli-box")
   ;
 
-/**
- * This function creates an ASCII box providing the width and height
- *
- */
-function createScreen (w, h) {
+// Constructor
+var TextAnimation = module.exports = function (options, callback) {
 
-    // left top corner
-    var screen = "╔";
-
-    // top line
-    for (var i = 0; i < w-2; ++i) {
-        screen += "═══";
+    // TextAnimation("foo", callback);
+    if (typeof options === "string") {
+        options = {
+            text: options
+        };
+    } else {
+        options = Object(options);
     }
 
-    // top right corner
-    screen += "╗";
-
-    // the other lines
-    for (var i = 0; i < h; ++i) {
-        screen += "\n║";
-        for (var ii = 0; ii < w - 2; ++ii) {
-            screen += "   ";
-        }
-        screen += "║";
-    }
-
-    // left bottom corner
-    screen += "\n╚";
-
-    // lasst line
-    for (var i = 0; i < w-2; ++i) {
-        screen += "═══";
-    }
-
-    // right bottom corner
-    screen += "╝";
-
-    // finally return the generated screen
-    return screen;
-}
-
-var TextAnimation = {};
-TextAnimation.start = function (options) {
-
-    // TODO checks
-
-    options.size = options.size.split("x");
-    options.size = {
-        width: options.size[0]
-      , height: options.size[1]
-    };
+    // Add the end handler
     options.end = options.end || function () {};
+    if (typeof callback === "function") {
+        options.end = callback;
+    }
 
-    var screen = createScreen (options.size.width, options.size.height)
-      , font = options.font || 'usaflag'
+    // Create the boxOptions
+    var boxOptions = {
+      , marks: {
+            nw: "╔"
+          , n:  "═"
+          , ne: "╗"
+          , e:  "║"
+          , se: "╝"
+          , s:  "═"
+          , sw: "╚"
+          , w:  "║"
+          , b: " "
+        }
+    };
+
+    options.animate = options.animate || "bottom-top";
+
+    // Handle the provided size
+    if (/^[0-9]+x[0-9]+$/.test(options.size)) {
+        options.size = options.size.split("x");
+        boxOptions.width = parseInt(options.size[0]);
+        boxOptions.height = parseInt(options.size[1]);
+    } else {
+        boxOptions.fullscreen = true;
+    }
+
+    var box = options.box = new Box(boxOptions)
+      , font = options.font || "usaflag"
       , text = options.text
       , frames = []
       ;
 
-    Jasciimo.write(text, font, function(art){
 
-        if (options.animate === "down-up") {
-            for (var y = options.size.height; y > 1; --y) {
-                frames.push(Overlap ({
-                    who: screen
-                  , with: art
-                  , where: {
-                        x: 3
-                      , y: y
-                    }
-                }));
-            }
+    // Render the text
+    Figlet.text(text, {
+        font: font,
+    }, function(err, data) {
+        if (err) {
+            return options.end(err, data);
         }
 
-        // create a new instance
+        options.rText = data;
+
+        try {
+            frames = require("./animations/" + options.animation)(options);
+        } catch (e) {
+
+        }
+
         var animation = new AsciiFrames ();
-
-        // load frames
         animation.loadFrames(frames);
-
-        // and start animation
         animation.startAnimation({
-            // 250 ms
             frameDelay: options.delay
         });
 
         setTimeout(function () {
-            options.end (frames);
+            options.end(frames);
         }, (frames.length + 1) * options.delay);
     });
 };
-
-module.exports = TextAnimation;
